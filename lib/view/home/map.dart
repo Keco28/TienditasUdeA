1778a9108cf8d0fc0f75d09a8e6c538b1158de6e
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-11-24 14:33:14 -0500

    Agregar dependeicas par marcadores y lista de marcadores

--- lib/view/home/map.dart
+++ lib/view/home/map.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 
 class UniversityMap extends StatelessWidget {
+
+  List<Marker> markers = [];
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +29,18 @@ class UniversityMap extends StatelessWidget {
       ),
     );
   }
-}
 
+
+  void loadMarkers(){
+    markers.add(
+      Marker(
+          width:40.0,
+          height: 40.0,
+          point: LatLng(0.0,0.0),
+          child: Icon(Icons.location_on, color: Colors.red),
+      ),
+    );
+  }
+
+}
 // assets/images/map.jpeg
