38df8c796c62a70836efb355e5666cabfecbb8c2
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-11-25 01:40:50 -0500

    cambios en el map para marcadores

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,4 +1,9 @@
+
+import "dart:ui";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+
+import "package:latlong2/latlong.dart";
 
 import "body.dart";
 
@@ -14,29 +19,31 @@ class HomeState extends State<Home> {
   double xOffset = 0;
   double yOffset = 0;
 
+
   bool isDrawerOpen = false;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar(),
       body: const Body(),
     );
   }
 
-  AppBar myAppBar() {
-    return AppBar(
-      foregroundColor: Colors.white,
-      backgroundColor: Colors.green,
-      elevation: 0,
-      title: const Text(
-        "Bienvenido a Tienditas UdeA",
-        style: TextStyle(
-          fontWeight: FontWeight.bold, // Optional: Adjust the font weight
+    AppBar myAppBar() {
+      return AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.green,
+        elevation: 0,
+        title: const Text(
+          "Bienvenido a Tienditas UdeA",
+          style: TextStyle(
+            fontWeight: FontWeight.bold, // Optional: Adjust the font weight
+          ),
         ),
-      ),
-      centerTitle: true, // Center the title
-    );
-  }
+        centerTitle: true, // Center the title
+      );
+    }
+
 }
 
--- lib/view/home/map.dart
+++ lib/view/home/map.dart
@@ -1,37 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:photo_view/photo_view.dart';
-import 'package:photo_view/photo_view_gallery.dart';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
-import 'package:image/image.dart' as img;
-import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:super_tooltip/super_tooltip.dart';
+
+
 
 class UniversityMap extends StatelessWidget {
 
-  List<Marker> markers = [];
-  Future<ui.Image> loadImage() async {
-    final ByteData data = await rootBundle.load('assets/images/map.jpeg');
-    final List<int> bytes = data.buffer.asUint8List();
-    final img.Image image = img.decodeImage(Uint8List.fromList(bytes))!;
-    final ui.Image imgui = await loadImageFromByteData(Uint8List.fromList(img.encodePng(image)));
-    return imgui;
-}
-
-  Future<ui.Image> loadImageFromByteData(Uint8List img) async {
-    final Completer<ui.Image> completer = Completer();
-    ui.decodeImageFromList(Uint8List.fromList(img), (ui.Image img) {
-      return completer.complete(img);
-    });
-    return completer.future;
-  }
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Container(
       // Adjust the size as needed
       width: MediaQuery.of(context).size.width,
@@ -45,18 +25,6 @@ class UniversityMap extends StatelessWidget {
       ),
     );
   }
-
-
-  void loadMarkers(){
-    markers.add(
-      Marker(
-          width:40.0,
-          height: 40.0,
-          point: LatLng(0.0,0.0),
-          child: Icon(Icons.location_on, color: Colors.red),
-      ),
-    );
-  }
-
 }
+
 // assets/images/map.jpeg
