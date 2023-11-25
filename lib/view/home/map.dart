583427c292e37991895683a06dc1fa54796f80ec
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-11-24 22:29:30 -0500

    convertir la imagen del mapa de la universidad en bytes

--- lib/view/home/map.dart
+++ lib/view/home/map.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -12,7 +16,21 @@ import 'package:flutter_svg/flutter_svg.dart';
 class UniversityMap extends StatelessWidget {
 
   List<Marker> markers = [];
+  Future<ui.Image> loadImage() async {
+    final ByteData data = await rootBundle.load('assets/images/map.jpeg');
+    final List<int> bytes = data.buffer.asUint8List();
+    final img.Image image = img.decodeImage(Uint8List.fromList(bytes))!;
+    final ui.Image imgui = await loadImageFromByteData(Uint8List.fromList(img.encodePng(image)));
+    return imgui;
+}
 
+  Future<ui.Image> loadImageFromByteData(Uint8List img) async {
+    final Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(Uint8List.fromList(img), (ui.Image img) {
+      return completer.complete(img);
+    });
+    return completer.future;
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
